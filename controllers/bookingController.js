f0cf348d817ee351c0db620669fe146ef3f4a3e2
angeliwanov <angel.iwanov@gmail.com>
2023-11-09 19:01:36 +0200

    bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,12 +58,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 
-const createBookingCheckout = async (session) => {
+const createBookingCheckout = catchAsync(async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
-};
+});
 
 exports.webhooksCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
